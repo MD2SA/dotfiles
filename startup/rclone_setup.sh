0bfc4540134fbb84540504d05c26c5dbb66a14cc
MD2SA <albuquerque2158@gmail.com>
2026-06-23 13:25:54 +0100

    fix(startup): quote paths and remove trailing backslash in rclone_setup

--- startup/rclone_setup.sh
+++ startup/rclone_setup.sh
@@ -5,7 +5,7 @@
 BISYNC_DIR="$HOME/.cache/rclone/bisync"
 rclone config
 
-rclone bisync $HOME/Pictures drive:omarchy/Pictures --resync -P
-rclone bisync $HOME/Documents drive:omarchy/Documents --resync -P \
-                                                      --exclude-if-present ".nobackup" \
-                                                      --exclude-if-present ".git" \
+rclone bisync "$HOME/Pictures" "drive:omarchy/Pictures" --resync -P
+rclone bisync "$HOME/Documents" "drive:omarchy/Documents" --resync -P \
+    --exclude-if-present ".nobackup" \
+    --exclude-if-present ".git"
